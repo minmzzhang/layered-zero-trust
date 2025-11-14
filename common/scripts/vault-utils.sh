c101c016475d932d61eeb15a88e2c9588fa16583
Min Zhang <minzhang@redhat.com>
2025-11-14 16:07:02 -0500

    feat: implement multi-role Vault JWT support from upstream
    Merge PR#69 changes to support multiple Vault JWT roles natively.
    
    Changes:
    1. Update common/scripts/vault-utils.sh (from validatedpatterns/layered-zero-trust#69)
       - Simplify to use vault_jwt_roles variable (matches rhvp.cluster_utils v1.1.0)
       - Remove legacy single-role variables (default_role, spiffe_audience, etc.)
       - Pass vault_jwt_roles instead of jwt_roles to Ansible
    
    2. Remove vault-role-creator workaround
       - Delete charts/rhtpa-operator/templates/vault-role-creator-job.yaml
       - Delete charts/rhtpa-operator/templates/vault-role-creator-script.yaml
       - No longer needed now that Ansible collection supports multi-role
    
    Upstream Dependencies:
    - rhvp.cluster_utils v1.1.0+ (PR validatedpatterns/rhvp.cluster_utils#61)
      - Implements vault_jwt_roles loop in vault_jwt_roles.yaml
      - Creates all JWT roles defined in values-hub.yaml
    - validatedpatterns/layered-zero-trust PR#69 (vault-utils.sh update)
    
    This properly creates all JWT roles (qtodo, rhtpa, syft) via Ansible
    instead of using the Kubernetes Job workaround.
    
    Testing:
    After installation, verify all roles created:
      oc exec -n vault vault-0 -- vault list auth/jwt/role
      # Expected: qtodo, rhtpa, syft
    
    Related: validatedpatterns/layered-zero-trust#69
    Related: validatedpatterns/rhvp.cluster_utils#61
    Signed-off-by: Min Zhang <minzhang@redhat.com>

--- common/scripts/vault-utils.sh
+++ common/scripts/vault-utils.sh
@@ -21,7 +21,7 @@ fi
 TASK="${1}"
 PATTERN_NAME=${2:-$(basename "`pwd`")}
 
-if [ -z ${TASK} ]; then
+if [ -z "${TASK}" ]; then
 	echo "Task is unset"
 	exit 1
 fi
@@ -31,27 +31,17 @@ EXTRA_PLAYBOOK_OPTS="${EXTRA_PLAYBOOK_OPTS:-}"
 if [ "$(yq ".clusterGroup.applications.vault.jwt.enabled // \"false\"" "${MAIN_CLUSTERGROUP_FILE}")" == "true" ]; then
   OCP_DOMAIN="$(oc get dns cluster -o jsonpath='{.spec.baseDomain}')"
   OIDC_DISCOVERY_URL="$(yq ".clusterGroup.applications.vault.jwt.oidcDiscoveryUrl" "${MAIN_CLUSTERGROUP_FILE}" | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
-  DEFAULT_ROLE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].name" "${MAIN_CLUSTERGROUP_FILE}")"
-  
-  # Extract all roles as JSON array and substitute clusterDomain
-  JWT_ROLES_JSON="$(yq -o json ".clusterGroup.applications.vault.jwt.roles" "${MAIN_CLUSTERGROUP_FILE}" | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
+  JWT_ROLES="$(yq -o json ".clusterGroup.applications.vault.jwt.roles" "${MAIN_CLUSTERGROUP_FILE}" | jq -rc | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
 
-  # Extract legacy variables from first role for backward compatibility
-  SPIFFE_AUDIENCE="$(yq ".clusterGroup.applications.vault.jwt.roles[0].audience" "${MAIN_CLUSTERGROUP_FILE}")"
-  SPIFFE_SUBJECT="$(yq ".clusterGroup.applications.vault.jwt.roles[0].subject" "${MAIN_CLUSTERGROUP_FILE}" | sed "s/{{ \$.Values.global.clusterDomain }}/${OCP_DOMAIN}/g")"
-  ROLE_POLICY="$(yq ".clusterGroup.applications.vault.jwt.roles[0].policies[0]" "${MAIN_CLUSTERGROUP_FILE}")"
-  TOKEN_TTL="$(yq ".clusterGroup.applications.vault.jwt.roles[0].ttl // \"86400\"" "${MAIN_CLUSTERGROUP_FILE}")"
-
-  if [ "${OIDC_DISCOVERY_URL}" == "null" ] || [ "${DEFAULT_ROLE}" == "null" ] || [ "${JWT_ROLES_JSON}" == "null" ]; then
+  if [ "${OIDC_DISCOVERY_URL}" == "null" ] || [ "${JWT_ROLES}" == "null" ] || [ "${JWT_ROLES}" == "[]" ]; then
     echo "Vault JWT config is disabled because of missing required fields"
     VAULT_JWT_CONFIG="false"
     echo "OIDC_DISCOVERY_URL: ${OIDC_DISCOVERY_URL}"
-    echo "DEFAULT_ROLE: ${DEFAULT_ROLE}"
+    echo "JWT_ROLES: ${JWT_ROLES}"
     echo "Vault JWT config is disabled"
   else
     VAULT_JWT_CONFIG="true"
     echo "Vault JWT config is enabled"
-    echo "Found $(echo "${JWT_ROLES_JSON}" | jq '. | length') JWT role(s) to configure"
   fi
 
 else
@@ -64,10 +54,5 @@ ansible-playbook -t "${TASK}" \
   -e pattern_dir="${PATTERNPATH}" \
   -e vault_jwt_config="${VAULT_JWT_CONFIG}" \
   -e oidc_discovery_url="${OIDC_DISCOVERY_URL:-}" \
-  -e default_role="${DEFAULT_ROLE:-}" \
-  -e spiffe_audience="${SPIFFE_AUDIENCE:-}" \
-  -e spiffe_subject="${SPIFFE_SUBJECT:-}" \
-  -e role_policy="${ROLE_POLICY:-}" \
-  -e token_ttl="${TOKEN_TTL:-86400}" \
-  -e jwt_roles="${JWT_ROLES_JSON:-[]}" \
+  -e vault_jwt_roles="${JWT_ROLES:-'[]'}" \
   ${EXTRA_PLAYBOOK_OPTS} "rhvp.cluster_utils.vault"
